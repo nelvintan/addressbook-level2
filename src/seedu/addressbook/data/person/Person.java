94dbb048a4c1e792f3452afe51b77fac6b8f4977
NELVINPC\HP <nelvintan@gmail.com>
2018-09-01 19:35:44 +0800

    add an instance-level member int sequenceNumber and a class-level variable int nextSequenceNumber to the Person class. [LO-ClassLevel]

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -12,6 +12,8 @@ import seedu.addressbook.data.tag.Tag;
  */
 public class Person implements ReadOnlyPerson {
 
+    private int sequenceNumber;
+    private static int nextSequenceNumber = 1;
     private Name name;
     private Phone phone;
     private Email email;
@@ -28,6 +30,8 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags.addAll(tags);
+        sequenceNumber = Person.nextSequenceNumber;
+        Person.nextSequenceNumber = Person.nextSequenceNumber + 1;
     }
 
     /**
